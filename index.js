cb2c88e8bf7ffc0189fdd3f0ab80b413d615d32e
oa-dev <arroyo.ot@gmail.com>
2021-07-12 17:25:40 +0100

    Create user and post data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const faker = require("faker");
 const { yourPath } = require("./config");
+const { lorem } = require("faker");
 
 const makeDir = (yourPathTo, directoryName) => {
   fs.mkdir(`${yourPathTo}${directoryName}`, { recursive: true }, (err) => {
@@ -8,3 +10,38 @@ const makeDir = (yourPathTo, directoryName) => {
 };
 
 // makeDir(yourPath, "fake-users");
+
+const generateUserInfo = () => {
+  const id = Math.random() * 1000;
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const userName = faker.internet.userName();
+  const avatar = faker.internet.avatar();
+  return { id, firstName, lastName, userName, avatar };
+};
+
+const generatePost = () => {
+  const vehicle = faker.vehicle.vehicle();
+  const colour = faker.vehicle.color();
+  const review = faker.lorem.paragraph();
+  return { vehicle, colour, review };
+};
+
+const mergeUserAndPostData = (amount, data = []) => {
+  while (data.length < amount) {
+    const { id, firstName, lastName, userName, avatar } = generateUserInfo();
+    const { vehicle, colour, review } = generatePost();
+    data.push({
+      id,
+      firstName,
+      lastName,
+      userName,
+      avatar,
+      vehicle,
+      colour,
+      review,
+    });
+  }
+  return data;
+};
+// console.log(mergeUserAndPostData(3));
